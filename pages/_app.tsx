67dd5f8545f7882788d5e0d800d83485d072d896
Shannon Yazdani <shannonyazdani@gmail.com>
2023-02-10 23:52:13 -0400

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+
+     return <Component {...pageProps} />
+
+}
