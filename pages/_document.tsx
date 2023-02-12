348a1f415b3c239eab7ae19dc2fc1a8bc2eb39ee
Shannon Yazdani <shannonyazdani@gmail.com>
2023-02-12 09:51:11 -0400

    add fonts to pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Bona+Nova:ital@0;1&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link href="https://fonts.googleapis.com/css2?family=Syncopate:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
