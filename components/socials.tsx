80e5258f94220a6c59a99c630f6378afa57753bd
Shannon Yazdani <shannonyazdani@gmail.com>
2023-02-13 11:39:03 -0400

    reduce quality

--- components/socials.tsx
+++ components/socials.tsx
@@ -12,23 +12,23 @@ function socials(){
         <ul className="flex lg:order-2 gap-2">
         <li>
             <a href="https://twitter.com/Layerrxyz" target={"_blank"} rel="noreferrer">
-        <Image placeholder="blur" src={twitter} alt="twitter" className='w-[30px] h-[30px] lg:w-[45px] lg:h-[45px]'></Image>
+        <Image placeholder="blur" quality={50} src={twitter} alt="twitter" className='w-[30px] h-[30px] lg:w-[45px] lg:h-[45px]'></Image>
             </a>
         </li>
         <li>
             <a href="https://discord.com/invite/RGe5w5EzjC" target={"_blank"} rel="noreferrer">
-        <Image placeholder="blur" src={discord} alt="discord" className='w-[30px] h-[30px]  lg:w-[45px] lg:h-[45px]'></Image>
+        <Image placeholder="blur" quality={50} src={discord} alt="discord" className='w-[30px] h-[30px]  lg:w-[45px] lg:h-[45px]'></Image>
             </a>
         </li>
         <li>
             <a href="https://www.instagram.com/layerrxyz/" target={"_blank"} rel="noreferrer">
-        <Image placeholder="blur" src={instagram} alt="instagram" className='w-[30px] h-[30px]  lg:w-[45px] lg:h-[45px]'></Image>
+        <Image placeholder="blur" quality={50} src={instagram} alt="instagram" className='w-[30px] h-[30px]  lg:w-[45px] lg:h-[45px]'></Image>
              </a>
        </li>
         </ul>
         <div className="flex items-center justify-center">
             <a href="https://www.layerr.xyz" target={"_blank"} rel="noreferrer">
-        <Image placeholder="blur" src={logo} alt="layerr logo" className="w-auto mt-2 lg:mt-0 order-1 h-[20px] md:h-[30px]  lg:h-[30px]"></Image>
+        <Image placeholder="blur" quality={50} src={logo} alt="layerr logo" className="w-auto mt-2 lg:mt-0 order-1 h-[20px] md:h-[30px]  lg:h-[30px]"></Image>
         </a>
         </div>
         </section>
